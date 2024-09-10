56dbb6ae854c828dc5e4fbdd3b60ad23c3474b53
lsj5677 <lsj5677@naver.com>
2024-09-10 13:53:02 -0400

    feat: add nav component

--- src/components/layout/nav.tsx
+++ src/components/layout/nav.tsx
@@ -1,32 +1,55 @@
+"use client";
+
 import { montserrat } from "@/styles/fonts";
 import Link from "next/link";
-import { FaRegUserCircle, FaUserCircle } from "react-icons/fa";
-import { IoMdAddCircle, IoMdAddCircleOutline } from "react-icons/io";
+import { usePathname } from "next/navigation";
+import { IoCreate, IoCreateOutline } from "react-icons/io5";
+import { RiUser5Line, RiUser5Fill } from "react-icons/ri";
 
-const menuItem = [
+type TMenuItems = {
+  href: string;
+  icon: React.ReactNode;
+  clickedIcon?: React.ReactNode;
+  title: string;
+};
+
+const menuItems = [
   {
-    href: "/",
-    icon: <IoMdAddCircleOutline />,
-    clickedIcon: <IoMdAddCircle />,
+    href: "/record",
+    icon: <IoCreateOutline />,
+    clickedIcon: <IoCreate />,
     title: "Record",
   },
   {
-    href: "/",
-    icon: <FaRegUserCircle />,
-    clickedIcon: <FaUserCircle />,
+    href: "/user",
+    icon: <RiUser5Line />,
+    clickedIcon: <RiUser5Fill />,
     title: "My page",
   },
 ];
 
 export default function Nav() {
+  const pathName = usePathname();
+
   return (
-    <div>
+    <div className="flex items-center justify-between bg-blue-200 p-4">
       <Link href="/" aria-label="Home">
         <h1 className={`text-2xl font-extralight ${montserrat.className}`}>
           <span className="font-semibold">PE</span>asy
           <span className="font-semibold">Fit</span>
         </h1>
       </Link>
+      <nav>
+        <ul className="flex items-center gap-5">
+          {menuItems.map(({ href, icon, clickedIcon, title }: TMenuItems) => (
+            <li key={href} className="text-4xl">
+              <Link href={href} aria-label={title}>
+                {pathName === href ? clickedIcon : icon}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </div>
   );
 }
