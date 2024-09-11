c2c85b0a35d7f852d9d292b4d95abf7c1651ce82
lsj5677 <lsj5677@naver.com>
2024-09-11 00:12:05 -0400

    etc: add open_sans weight

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,6 +1,9 @@
 import { Montserrat, Nanum_Gothic, Open_Sans } from "next/font/google";
 
-const open_sans = Open_Sans({ subsets: ["latin"] });
+const open_sans = Open_Sans({
+  weight: ["300", "400", "700"],
+  subsets: ["latin"],
+});
 const nanum_gothic = Nanum_Gothic({
   weight: ["400", "700", "800"],
   style: ["normal"],
