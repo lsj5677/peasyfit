98af69550fd9a19859fcdcad9a795737f8f560e0
lsj5677 <lsj5677@naver.com>
2024-09-10 21:36:02 -0400

    refactor: delete signout, add session

--- src/components/layout/nav.tsx
+++ src/components/layout/nav.tsx
@@ -1,38 +1,19 @@
 "use client";
 
-import { montserrat } from "@/styles/fonts";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { IoCreate, IoCreateOutline } from "react-icons/io5";
-import { RiUser5Line, RiUser5Fill } from "react-icons/ri";
 import Logo from "../ui/Logo";
-
-type TMenuItems = {
-  href: string;
-  icon: React.ReactNode;
-  clickedIcon?: React.ReactNode;
-  title: string;
-};
-
-const menuItems = [
-  {
-    href: "/record",
-    icon: <IoCreateOutline />,
-    clickedIcon: <IoCreate />,
-    title: "Record",
-  },
-  {
-    href: "/user",
-    icon: <RiUser5Line />,
-    clickedIcon: <RiUser5Fill />,
-    title: "My page",
-  },
-];
+import LinkButton from "../ui/LinkButton";
+import ActionButton from "../ui/ActionButton";
 
 export default function Nav() {
   const pathName = usePathname();
   const { data: session } = useSession();
+  const user = session?.user;
+
+  console.log(user);
 
   return (
     <div className="bg-subOrange flex items-center justify-between p-4">
@@ -41,31 +22,26 @@ export default function Nav() {
       </Link>
       <nav>
         <ul className="flex items-center gap-5">
-          {menuItems.map(({ href, icon, clickedIcon, title }: TMenuItems) => (
-            <li key={href} className="text-4xl">
-              <Link href={href} aria-label={title}>
-                {pathName === href ? clickedIcon : icon}
-              </Link>
-            </li>
-          ))}
-          {/* href 에서 조건 처리해주기 (session 없으면 login page로) */}
-          {/* test */}
-          {session ? (
-            <button
-              onClick={() => {
-                signOut();
-              }}
-            >
-              Sign out
-            </button>
+          <li className="text-4xl">
+            <Link href={"/record"} aria-label="Record">
+              {pathName === "/record" ? <IoCreate /> : <IoCreateOutline />}
+            </Link>
+          </li>
+          {session && user ? (
+            <>
+              <li>
+                <Link href={`/user/${user.username}`}>
+                  <LinkButton text="My Page" />
+                </Link>
+              </li>
+            </>
           ) : (
-            <button
+            <ActionButton
+              text="Sign In"
               onClick={() => {
                 signIn();
               }}
-            >
-              Sign in
-            </button>
+            />
           )}
         </ul>
       </nav>
