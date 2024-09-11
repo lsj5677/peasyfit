5767130829ad6f5bc438b69e66b78afd88ed09da
lsj5677 <lsj5677@naver.com>
2024-09-11 00:13:49 -0400

    refactor: add xl size

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,14 +1,20 @@
 import { montserrat } from "@/styles/fonts";
 
 type TLogo = {
-  size?: "sm" | "md" | "lg";
+  size?: "md" | "lg" | "xl";
   className?: string;
 };
 export default function Logo({ size = "lg", className }: TLogo) {
   return (
     <h1
       className={`text-subPurple ${
-        size === "lg" ? "text-2xl" : size === "md" ? "text-lg" : "text-md"
+        size === "xl"
+          ? "text-4xl"
+          : size === "lg"
+            ? "text-2xl"
+            : size === "md"
+              ? "text-lg"
+              : "text-md"
       } font-light ${montserrat.className} ${className}`}
     >
       <span className="text-mainPurple font-bold">PE</span>asy
