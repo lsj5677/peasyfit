1635f1a8e552623b099b8f8ac8f43cd77cda6864
lsj5677 <lsj5677@naver.com>
2024-09-20 02:59:56 -0400

    etc: parseDate 및 코드수정(refactor)

---
+++ src/utils/parseDate.ts
@@ -0,0 +1,8 @@
+export function parseDate(dateString: string) {
+  const dateParts = dateString.split(/[., ]+/);
+  const year = dateParts[0];
+  const month = dateParts[1].padStart(2, "0"); // 2자리로 맞추기
+  const day = dateParts[2].padStart(2, "0"); // 2자리로 맞추기
+
+  return `${month}/${day}`; // yy/mm/dd 형식으로 반환
+}
